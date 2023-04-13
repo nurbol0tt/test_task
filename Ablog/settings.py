d59cebf675be13bd982580a698c27e95ceab2a8f
nurbol0tt <fairtail764@gmail.com>
2023-04-13 22:32:12 +0600

    fix bug

--- Ablog/settings.py
+++ Ablog/settings.py
@@ -121,10 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
