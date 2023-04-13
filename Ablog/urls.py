463d8c5bd60164ce278391971d3fee7aea9ed57a
nurbol0tt <fairtail764@gmail.com>
2023-04-13 22:06:01 +0600

    modify urls

--- Ablog/urls.py
+++ Ablog/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('user/', include('django.contrib.auth.urls')),
     path('user/', include('user.urls')),
 
